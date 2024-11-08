4dd9660b5c75cff40147a84e8b9b59e040f672a7
Talin <viridia@gmail.com>
2024-11-07 18:55:49 -0800

    Auto-registration of effect cleanup hook.

--- crates/thorium_ui_core/src/effect_cell.rs
+++ crates/thorium_ui_core/src/effect_cell.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, Mutex};
 
 use bevy::{
-    ecs::{system::SystemId, world::DeferredWorld},
+    ecs::{component::ComponentId, system::SystemId, world::DeferredWorld},
     prelude::*,
     ui::experimental::GhostNode,
 };
@@ -11,6 +11,7 @@ use bevy::{
 /// Note: If Bevy had trait queries, we wouldn't the Arc/Mutex.
 #[derive(Component)]
 #[require(GhostNode)]
+#[component(on_remove = on_remove_effect)]
 pub struct EffectCell(pub(crate) Arc<Mutex<dyn AnyEffect + 'static + Sync + Send>>);
 
 impl EffectCell {
@@ -22,6 +23,21 @@ impl EffectCell {
 pub(crate) trait AnyEffect {
     fn update(&mut self, world: &mut World, entity: Entity);
     fn cleanup(&self, world: &mut DeferredWorld, entity: Entity);
+    // fn is_changed(
+    //     &self,
+    //     _world: &mut DeferredWorld,
+    //     _entity: Entity,
+    //     _last_run: Tick,
+    //     _this_run: Tick,
+    // ) -> bool {
+    //     false
+    // }
+}
+
+fn on_remove_effect(mut world: DeferredWorld, entity: Entity, _cid: ComponentId) {
+    let cell = world.get_mut::<EffectCell>(entity).unwrap();
+    let comp = cell.0.clone();
+    comp.lock().unwrap().cleanup(&mut world, entity);
 }
 
 pub(crate) fn update_effects(world: &mut World) {
--- crates/thorium_ui_core/src/lib.rs
+++ crates/thorium_ui_core/src/lib.rs
@@ -4,13 +4,14 @@ mod effect_cell;
 mod entity_effect;
 mod foreach;
 mod lcs;
+// mod memo;
 mod switch;
 mod template;
 
 use bevy::app::{App, Plugin, Update};
 pub use callback::CreateCallback;
 pub use cond::Cond;
-use effect_cell::{update_effects, EffectCell};
+use effect_cell::update_effects;
 pub use entity_effect::{EntitEffect, WithEffect};
 pub use foreach::ForEach;
 pub use switch::Switch;
@@ -21,12 +22,5 @@ pub struct ThoriumUiPlugin;
 impl Plugin for ThoriumUiPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, update_effects);
-        app.world_mut()
-            .register_component_hooks::<EffectCell>()
-            .on_remove(|mut world, entity, _cond| {
-                let cell = world.get_mut::<EffectCell>(entity).unwrap();
-                let comp = cell.0.clone();
-                comp.lock().unwrap().cleanup(&mut world, entity);
-            });
     }
 }
