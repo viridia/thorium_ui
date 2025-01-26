0dc2873bc76660e4d42a5cf129e44d5d432552e1
Talin <viridia@gmail.com>
2025-01-26 11:43:59 -0800

    Reverted changes to cond.

--- crates/thorium_ui_core/src/cond.rs
+++ crates/thorium_ui_core/src/cond.rs
@@ -6,7 +6,7 @@ use bevy::{
         world::DeferredWorld,
     },
     prelude::*,
-    // ui::experimental::GhostNode,
+    ui::experimental::GhostNode,
 };
 
 use crate::{
@@ -110,7 +110,7 @@ impl<
     fn apply(self, entity: &mut EntityWorldMut) {
         let test_sys = unsafe { entity.world_mut().register_system(self.test_fn) };
         entity.insert((
-            EffectCell::new(CondEffect {
+            EffectCell::new(CondEffect2 {
                 state: false,
                 first: true,
                 test_sys,
@@ -124,8 +124,97 @@ impl<
     }
 }
 
+pub trait CreateCond {
+    fn cond<
+        M: Send + Sync + 'static,
+        TestFn: IntoSystem<(), bool, M> + Send + Sync + 'static,
+        Pos: Fn(&mut ChildSpawnerCommands) + Send + Sync + 'static,
+        Neg: Fn(&mut ChildSpawnerCommands) + Send + Sync + 'static,
+    >(
+        &mut self,
+        test: TestFn,
+        pos: Pos,
+        neg: Neg,
+    ) -> &mut Self;
+}
+
+impl CreateCond for ChildSpawnerCommands<'_> {
+    fn cond<
+        M: Send + Sync + 'static,
+        TestFn: IntoSystem<(), bool, M> + Send + Sync + 'static,
+        Pos: Fn(&mut ChildSpawnerCommands) + Send + Sync + 'static,
+        Neg: Fn(&mut ChildSpawnerCommands) + Send + Sync + 'static,
+    >(
+        &mut self,
+        test_fn: TestFn,
+        pos: Pos,
+        neg: Neg,
+    ) -> &mut Self {
+        // let test_sys = self.commands().register_system(test);
+        let mut ent = self.spawn_empty();
+        let test_sys = ent.commands().register_system(test_fn);
+        ent.insert((
+            EffectCell::new(CondEffect {
+                state: false,
+                first: true,
+                test_sys,
+                pos,
+                neg,
+                marker: std::marker::PhantomData::<M>,
+            }),
+            GhostNode::default(),
+            Fragment,
+        ));
+        self
+    }
+}
+
 /// Conditional control-flow node.
-struct CondEffect<
+struct CondEffect<M, Pos: Fn(&mut ChildSpawnerCommands), Neg: Fn(&mut ChildSpawnerCommands)> {
+    state: bool,
+    first: bool,
+    test_sys: SystemId<(), bool>,
+    pos: Pos,
+    neg: Neg,
+    marker: std::marker::PhantomData<M>,
+}
+
+impl<M, Pos: Fn(&mut ChildSpawnerCommands), Neg: Fn(&mut ChildSpawnerCommands)> AnyEffect
+    for CondEffect<M, Pos, Neg>
+{
+    fn update(&mut self, world: &mut World, entity: Entity) {
+        // Run the condition and see if the result changed.
+        let test = world.run_system(self.test_sys);
+        if let Ok(test) = test {
+            if self.state != test || self.first {
+                self.first = false;
+                let mut entt = world.entity_mut(entity);
+                // entt.despawn_related::<Children>();
+                entt.despawn_related::<DynChildren>();
+                entt.despawn_related::<Owned>();
+                let mut commands = world.commands();
+                let mut entt = commands.entity(entity);
+                if test {
+                    entt.with_children(|builder| {
+                        (self.pos)(builder);
+                    });
+                } else {
+                    entt.with_children(|builder| {
+                        (self.neg)(builder);
+                    });
+                }
+                self.state = test;
+            }
+        }
+    }
+
+    fn cleanup(&self, world: &mut DeferredWorld, _entity: Entity) {
+        world.commands().unregister_system(self.test_sys);
+    }
+}
+
+/// Conditional control-flow node.
+struct CondEffect2<
     M,
     PosBundle: SpawnableList<DynChildOf>,
     Pos: Fn() -> PosBundle,
@@ -146,7 +235,7 @@ impl<
         Pos: Fn() -> PosBundle,
         NegBundle: SpawnableList<DynChildOf>,
         Neg: Fn() -> NegBundle,
-    > AnyEffect for CondEffect<M, PosBundle, Pos, NegBundle, Neg>
+    > AnyEffect for CondEffect2<M, PosBundle, Pos, NegBundle, Neg>
 {
     fn update(&mut self, world: &mut World, entity: Entity) {
         // Run the condition and see if the result changed.
